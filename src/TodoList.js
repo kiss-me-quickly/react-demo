99bb25c586b7286d735c66534e761756a29c4dd7
mirror <im.mirror@live.cn>
2019-10-16 10:21:35 +0800

    练习JSX

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,50 +1,58 @@
-import React, { Component , Fragment} from 'react';
+import React, { Component, Fragment } from 'react'
+import './index.css'
 
 class TodoList extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            inputValue: '',
-            list: []
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+      list: []
     }
-    render() {
-        return (
-            <Fragment>
-                <div>
-                    <input value={this.state.inputValue}
-                           onChange={this.handleInputChange.bind(this)}
-                    />
-                    <button onClick={this.handleSubmit.bind(this)}>提交</button>
-                </div>
-                <ul>
-                    {
-                        this.state.list.map((item, index) => {
-                            return <li key={'key'+index} onClick={this.handleDelete.bind(this, index)}>{item}</li>
-                        })
-                    }
-                </ul>
-            </Fragment>
-        )
-    }
-    handleInputChange(e) {
-        this.setState({
-            inputValue: e.target.value
-        })
-    }
-    handleSubmit() {
-        this.setState({
-            list: [...this.state.list, this.state.inputValue],
-            inputValue: ''
-        })
-    }
-    handleDelete(index) {
-        const list = [...this.state.list];
-        list.splice(index, 1)
-        this.setState({
-            list
-        })
-    }
+  }
+
+  render () {
+    return (
+      <Fragment>
+        <div>
+          <label htmlFor='insertArea'>输入内容</label>
+          <input className='input' id='insertArea' value={this.state.inputValue}
+                 onChange={this.handleInputChange.bind(this)}
+          />
+          <button onClick={this.handleSubmit.bind(this)}>提交</button>
+        </div>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+              return <li key={'key' + index}
+                         dangerouslySetInnerHTML={{ __html: item }}
+                         onClick={this.handleDelete.bind(this, index)}/>
+            })
+          }
+        </ul>
+      </Fragment>
+    )
+  }
+
+  handleInputChange (e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  handleSubmit () {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  handleDelete (index) {
+    const list = [...this.state.list]
+    list.splice(index, 1)
+    this.setState({
+      list
+    })
+  }
 }
 
-export default TodoList;
+export default TodoList
